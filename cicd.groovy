bde55aa3720343e8b62420b3438f6616412619dd
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:03 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/re2cport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/re2cport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/re2cport.git'), string(name: 'PORT_DESCRIPTION', value: 're2c is a free and open-source lexer generator for C/C++, Go and Rust.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/re2cport.git'), string(name: 'PORT_DESCRIPTION', value: 're2c is a free and open-source lexer generator for C/C++, Go and Rust.' )]
   }
 }
